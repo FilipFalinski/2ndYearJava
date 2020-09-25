c791baee18a8ae1f648cadf141030c3a4814741c
FilipFalinski <55143687+FilipFalinski@users.noreply.github.com>
2020-09-25 12:34:53 +0100

    Add files via upload

--- 2ndJavaRepeat/src/Lab1/Exercise4.java
+++ 2ndJavaRepeat/src/Lab1/Exercise4.java
@@ -3,10 +3,27 @@ package Lab1;
 import javax.swing.*;
 import java.util.Scanner;
 
+
 public class Exercise4 {
+    
+    
 
     public static void main(String[] args) {
 
+        int distance = 10;
+        double rate = 1.75 ;
+        double excess = 2.50;
+        
+        
+            if(distance <= 10) {
+
+                rate = 1.75 * distance;
+            }
+            else{
+                if(distance > 10){
+                    excess= rate+distance*2.50;
+                }
+            }
 
         Scanner myObj = new Scanner(System.in);
 
@@ -18,7 +35,9 @@ public class Exercise4 {
                 null, "Please enter the number of km cycled");
         System.out.println(Distance);
 
-        JOptionPane.showMessageDialog(null, "Name:" + name +"\nDistance Cycled:" + Distance);
+
+            
+        JOptionPane.showMessageDialog(null, "Name:" + name +"\nDistance Cycled:" + Distance +"\nSponsorship amount:" + rate + excess );
 
 
     }
